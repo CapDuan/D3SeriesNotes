70facfef984e92d31a29a536905c1245e347193d
wenchao duan <276293964@qq.com>
2018-01-31 15:05:05 +0800

    add the new files

--- front_src/series/series1/series1.js
+++ front_src/series/series1/series1.js
@@ -2,10 +2,49 @@
  * Created by wenchao on 2018/1/25.
  */
 window.onload = function () {
-    log.debug(window);
-    log.debug(d3.select("body"));
 
+    var dataset = [];
+    for (var i = 0; i < 20; i++) {
+        dataset.push(i);
+    }
+    var svg_width = 1860;
+    var svg_height = 640;
+    //svg
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", svg_width)
+        .attr("height", svg_height);
+    // circle
+    var circles = svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle");
+
+    circles.attr("cx", function (d, i) {
+        return i * 50 + 25;
+    }).attr("cy", function (d, i) {
+        return (svg_height / 2);
+    }).attr("r", function (d) {
+        return (d * 1.2) + 15
+    }).style("fill", function (d, i) {
+        log.debug(d, i);
+        return d3.rgb(213, 255 - (i * 15), i * 15, 0.9);
+    });
+
+    var text = svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(function (d, i) {
+            return d;
+        }).attr("x", function (d, i) {
+            return i * 50 + 25;
+        }).attr("y", function (d, i) {
+            return (svg_height / 2 ) + 5;
+        });
+    text.attr("fill", "white").attr("text-anchor", "middle");
 }
+
 
 /** 2018/1/25 - 11:38
  *---------------------------------
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,20 @@
  * Created by wenchao on 2018/1/25.
  */
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function () {
-    console.log("default task start: ---");
-})
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        startPath: './front_src/series/series1/series1.html'
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./front_src/series/series1/series1.html', browserSync.reload);
+    gulp.watch('./front_src/series/series1/series1.js', browserSync.reload);
+});
+gulp.task('default', ['browser-sync', 'watch']);
